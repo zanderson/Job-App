88c92e3bc6703b5bfb3d662391ecdca3bcc4eb05
Student <student@RU-012.local>
2016-02-23 11:39:41 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Create Express App Object \\
+var app = express();
+
+// Application Configuration \\
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+
+app.get('/', function(req, res) {
+	res.sendFile('html/index.html', {root : './public'});
+});
+
+// displays a list of applicants
+app.get('/applicants', function(req, res){
+	res.sendFile('html/applicants.html', {root : './public'});
+});
+
+// creates and applicant
+app.post('/applicant', function(req, res){
+	// Here is where you need to get the data
+	// from the post body and store it in the database
+	res.send('No funciona');
+});
+
+
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+angular.module('JobApp', [])
+
+angular.module('JobApp')
+	.controller('homeController', ['$scope', function($scope){
+		
+	}]);
+
+angular.module('JobApp')
+	.controller('applicantController', ['$scope', function($scope){
+		
+	}]);
